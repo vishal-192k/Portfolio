01a471ded06a094038a7bd13ad46d339ac6184f2
Vishal Balasaheb Khile <vbkhile192@gmail.com>
2024-06-26 15:58:50 +0530

    homepage design done

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from "../../assets/profile-imgg.png"
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt='' />
+        <h4> <span> I'm vishal khile </span>frontend developer</h4>
+        <p>As a skilled front-end developer from India, I specialize in crafting responsive, user-friendly web interfaces.I strive to deliver exceptional digital experiences..</p>
+        <div className="hero-action">
+             <div className="hero-resume">Resume</div>
+             <div className="hero-connect">Connect</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
