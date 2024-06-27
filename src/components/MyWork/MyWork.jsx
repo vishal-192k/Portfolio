9c283bd02e93d0f3a6c4c27237a9adb881fee3ae
Vishal Balasaheb Khile <vbkhile192@gmail.com>
2024-06-27 18:47:50 +0530

    mywork style part completed

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -17,10 +17,10 @@ export default function MyWork() {
                 return <img  key={index} src={work.w_img} />
             })}
         </div>
-        <div className="mywork-showmore">
+        {/* <div className="mywork-showmore">
             <p>Show More</p>
             <img src={arrow_icon} alt="" />
-        </div>
+        </div> */}
       
     </div>
   )
